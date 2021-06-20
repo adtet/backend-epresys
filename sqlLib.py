f4b468f00d2550e7d6a22711f4df1e125d0d397a
Adiyatma Koto <adiyatma.padang@gmail.com>
2021-06-21 00:19:00 +0700

    adit commit repair sql belum beres

--- sqlLib.py
+++ sqlLib.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import json
 
+from mysql.connector import cursor
+
 def koneksi_sql():
     sql = mysql.connector.connect(host="localhost",user="root",password="",database="db_coba")
     return sql
@@ -22,19 +24,50 @@ def cek_user(username,password):
         return c[0]
 
 
-def get_jadwal(kelas,day):
+def get_jadwal(kelas):
     db = koneksi_sql()
     cursor = db.cursor()
-    cursor.execute("SELECT  `jamstart`, `menitstart`, `jamend`, `menitend`, `matakuliah`, `dosen`  FROM `schedule` WHERE `kelas`=%s AND `day`=%s",(kelas,day))
-    rows = [x for x in cursor]  #compare sql data to json
-    cols = [x[0] for x in cursor.description]  #compare sql data to json
-    datas = []  #compare sql data to json
-    for row in rows:  #compare sql data to json
-        data = {}  #compare sql data to json
-        for prop, val in zip(cols, row):  #compare sql data to json
-            data[prop] = val  #compare sql data to json
-        datas.append(data)  #compare sql data to json
-    dataJson = json.dumps(datas)  #compare sql data to json
+    try:
+        cursor.execute("SELECT  `time_start`, `time_end`,  `matakuliah`, `dosen1`, `dosen2`, `dosen3`, `ruangan` FROM `schedule` WHERE `kelas`=%s AND `day`=LOWER(DAYNAME(CURDATE()))",(kelas,"monday"))
+        rows = [x for x in cursor]  
+        cols = [x[0] for x in cursor.description]
+    except(mysql.connector.Error,mysql.connector.Warning) as e:
+        print(e)
+        rows = []
+        cols = []  
+    datas = []  
+    for row in rows:  
+        data = {}  
+        for prop, val in zip(cols, row):  
+            data[prop] = val  
+        datas.append(data)  
+    for x in range(0,len(datas)):
+        datas[x]['time_start'] = str(datas[x]['time_start'])
+        datas[x]['time_end'] = str(datas[x]['time_end'])
+    dataJson = json.dumps(datas)  
+    return dataJson
+
+def get_jadwal_dosen(nip):
+    db = koneksi_sql()
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT `kelas`,`time_start`, `time_end`, `matakuliah`,`ruangan` FROM `schedule` WHERE `day` = LOWER(DAYNAME(CURDATE())) AND `kode_dosen1`= %s OR `kode_dosen2`= %s OR `kode_dosen3`=%s",(nip,nip,nip))
+        rows = [x for x in cursor]  
+        cols = [x[0] for x in cursor.description]
+    except(mysql.connector.Error,mysql.connector.Warning) as e:
+        print(e)
+        rows = []
+        cols = []  
+    datas = []  
+    for row in rows:  
+        data = {}  
+        for prop, val in zip(cols, row):  
+            data[prop] = val  
+        datas.append(data)  
+    for x in range(0,len(datas)):
+        datas[x]['time_start'] = str(datas[x]['time_start'])
+        datas[x]['time_end'] = str(datas[x]['time_end'])
+    dataJson = json.dumps(datas)  
     return dataJson
 
 def get_kelas(a):
@@ -64,12 +97,49 @@ def get_main(a):
     dataJson = json.dumps(datas)  #compare sql data to json
     return dataJson  #compare sql data to json
 
+def get_main_2(id):
+    db = koneksi_sql()
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT `matakuliah`, `kelas`, `day`, `date`, `time`, `info` FROM `main2` WHERE id=%s",(id,))
+        rows = [x for x in cursor]  
+        cols = [x[0] for x in cursor.description]
+    except(mysql.connector.Error,mysql.connector.Warning) as e:
+        print(e)
+        rows = []
+        cols = []  
+    datas = []  
+    for row in rows:  
+        data = {}  
+        for prop, val in zip(cols, row):  
+            data[prop] = val  
+        datas.append(data)  
+    for x in range(0,len(datas)):
+        datas[x]['time'] = str(datas[x]['time'])
+        datas[x]['date'] = str(datas[x]['date'])
+    dataJson = json.dumps(datas)  
+    return dataJson
+
 def cek_id_main(a):
     db = koneksi_sql()
     cursor = db.cursor()
     cursor.execute("SELECT id FROM main where id=%s", (a, ))
     c = cursor.fetchall()
     if c == None:
+        return False
+    else:
+        return True
+
+def cek_id_main_dosen(id):
+    db = koneksi_sql()
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT  `id` FROM `main2` WHERE `id`=%s",(id,))
+        c = cursor.fetchone()
+    except(mysql.connector.Warning,mysql.connector.Error) as e:
+        print(e)
+        c = None
+    if c==None:
         return False
     else:
         return True
@@ -106,10 +176,10 @@ def get_nim(a):
         return c[0]
 
 
-def get_jurusan(a):
+def get_username(id):
     db = koneksi_sql()
     cursor = db.cursor()
-    cursor.execute("SELECT jurusan FROM user_regist where id=%s", (a, ))
+    cursor.execute("SELECT username FROM user_regist where id=%s", (id, ))
     c = cursor.fetchone()
     if c == None:
         return 0
@@ -117,59 +187,52 @@ def get_jurusan(a):
         return c[0]
 
 
-def get_prodi(a):
-    db = koneksi_sql()
-    cursor = db.cursor()
-    cursor.execute("SELECT prodi FROM user_regist where id=%s", (a, ))
-    c = cursor.fetchone()
-    if c == None:
-        return 0
-    else:
-        return c[0]
-
-def get_username(a):
-    db = koneksi_sql()
-    cursor = db.cursor()
-    cursor.execute("SELECT username FROM user_regist where id=%s", (a, ))
-    c = cursor.fetchone()
-    if c == None:
-        return 0
-    else:
-        return c[0]
-
-
-def get_matkul(a, b, c):
+def get_matkul(kelas):
     db = koneksi_sql()
     cursor = db.cursor()
     cursor.execute(
-        "SELECT matakuliah,dosen FROM schedule where kelas=%s and day=%s and time>=%s and end>=%s",
-        (a, b, c, c))
+        "SELECT `matakuliah`, `dosen1`, `dosen2`,`dosen3` FROM `schedule` WHERE `kelas`=%s AND `day`= LOWER(DAYNAME(CURDATE())) AND time_start>=CURTIME() AND time_end>= CURTIME()",
+        (kelas,))
     d = cursor.fetchone()
     if d == None:
-        return 0
+        return None
     else:
         return d
 
-def get_matkul_late(a, b, c):
+def get_matkul_late(kelas):
     db = koneksi_sql()
     cursor = db.cursor()
     cursor.execute(
-        "SELECT matakuliah,dosen FROM schedule where kelas=%s and day=%s and time<=%s and end>=%s",
-        (a, b, c, c))
+        "SELECT `matakuliah`, `dosen1`, `dosen2`,`dosen3` FROM `schedule` WHERE `kelas`=%s AND `day`= LOWER(DAYNAME(CURDATE())) AND time_start<=CURTIME() AND time_end>= CURTIME()",
+        (kelas,))
     d = cursor.fetchone()
     if d == None:
-        return 0
+        return None
     else:
         return d
 
-def cek_present(a, b, c):
+def cek_present(id,matakuliah):
     db = koneksi_sql()
     cursor = db.cursor()
     cursor.execute(
-        "SELECT id FROM main where id=%s and matakuliah=%s ,date=%s",
-        (a, b, c))
+        "SELECT id FROM main where id=%s and matakuliah=%s and date= now()",
+        (id,matakuliah))
     d = cursor.fetchone()
     if d == None:
+        return True
+    else:
+        return False
+
+def cek_present_dosen(id,matakuliah,date):
+    db = koneksi_sql()
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT `id` FROM `main2` WHERE id=%s AND matakuliah=%s AND `date` = now()",(id,matakuliah))
+        c = cursor.fetchone()
+    except(mysql.connector.Warning,mysql.connector.Error) as e:
+        print(e)
+        c = None
+    if c ==None:
         return True
     else:
         return False
@@ -184,14 +247,17 @@ def cek_id(a):
     else:
         return True
 
-def insert_main(a, b, c, d, e, f, g, h, i, j, k, l, m):
+def insert_main(id, nim, username, kelas, matakuliah, dosen1, dosen2, dosen3, info):
     db = koneksi_sql()
     cursor = db.cursor()
     cursor.execute(
-    "INSERT INTO `main`(`id`, `nim`, `username`, `jurusan`, `prodi`, `kelas`, `email`, `matakuliah`, `dosen`,`day`,`date` ,`time`, `info`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-    (a, b, c, d, e, f, g, h, i, j, k, l, m))
+    "INSERT INTO `main`(`id`, `nim`, `username`, `kelas`, `matakuliah`, `dosen1`, `dosen2`, `dosen3`, `day`, `date`, `time`, `info`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,LOWER(DAYNAME(now())),now(),now(),%s)",(id, nim, username, kelas, matakuliah, dosen1, dosen2, dosen3, info))
     db.commit()
 
+def insert_main_dosen():
+    db = koneksi_sql()
+    
+    
 def cek_present(a, b, c):
     db = koneksi_sql()
     cursor = db.cursor()
@@ -203,6 +269,3 @@ def cek_present(a, b, c):
         return True
     else:
         return False
-
-
-       
